1c6f0e789f8b471ae106a9e9106d01bcdcb08500
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2023-11-13 14:22:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,33 +6,31 @@
 
 int contador=0;
 
-struct Cliente {
+typedef struct {    
     char cpf[12];
-    char nome[50];
+    char nome[51];
     int idade;
     int dia, mes, ano;
-};
+} Cliente;
 
-struct Animal{
+typedef struct {
     int codigo;
-    char nome[50];
+    char nome[51];
     int idade;
     char cor[10];
     char status[10];
-};
+} Animal;
 bool verificaCPF(char cpf[]) {
     // Verifica se o CPF tem 11 dígitos
     if (strlen(cpf) != 11) {
         return true;
     }
-
     // Verifica se todos os caracteres são dígitos
     for (int i = 0; i < 11; i++) {
         if (!isdigit(cpf[i])) {
             return true;
         }
     }
-
     // Calcula o primeiro dígito verificador
     int soma = 0;
     for (int i = 0; i < 9; i++) {
@@ -42,12 +40,10 @@ bool verificaCPF(char cpf[]) {
     if (digito1 > 9) {
         digito1 = 0;
     }
-
     // Verifica o primeiro dígito verificador
     if (digito1 != cpf[9] - '0') {
         return true;
     }
-
     // Calcula o segundo dígito verificador
     soma = 0;
     for (int i = 0; i < 10; i++) {
@@ -57,98 +53,170 @@ bool verificaCPF(char cpf[]) {
     if (digito2 > 9) {
         digito2 = 0;
     }
-
     // Verifica o segundo dígito verificador
     if (digito2 != cpf[10] - '0') {
         return true;
     }
-
     // Se passou por todas as verificações, o CPF é válido
     return false;
 }
 void cadastrarCliente(){
   int n;
-    printf("Cadastrando Clientes :\n");
-    printf("Quantos clientes deseja cadastrar: ");
-    scanf("%i", &n);
+  printf("----------------------------------------\n");
+  printf("Cadastrando Clientes :\n");
+  printf("Quantos clientes deseja cadastrar: ");
+  scanf("%i", &n);
 
     // Alocação dinâmica
-    struct Cliente *cl = (struct Cliente *)malloc(n * sizeof(struct Cliente)); 
+    Cliente *cl = (Cliente *)malloc(n * sizeof(Cliente)); 
 
     if (cl == NULL) {
         printf("Falha na alocação de memória.\n");
-        return 1;
+        return;
     }
-
-
     FILE *cli;
     cli=fopen("clientes.txt", "a" );
         if (cli == NULL) {
             printf("Erro na abertura do arquivo !");
             system("pause");
             exit(1);
-        }
-
+       }
 
         for (int i = 0; i < n; i++) {
             while (verificaCPF(cl[i].cpf))
             {
-                printf("Digite o seu CPF: \n");
-            fflush(stdin);
-            fgets(cl[i].cpf, 12, stdin);
+            printf("Digite o seu CPF: \n");
+            scanf("%s",cl[i].cpf);            
             verificaCPF(cl[i].cpf);
             if (verificaCPF(cl[i].cpf)) {
                 printf("CPF invalido e/ou incorreto!\n");
             } else {
                 printf("CPF valido!\n");
                 }   
-            } 
-
-
-            printf("Digite o nome do Cliente: \n");
-            fflush(stdin);
-            fgets(cl[i].nome, 50, stdin);
+            }
+            printf("Digite o nome do Cliente: \n");      
+            scanf("%s",cl[i].nome);
             printf("Digite a idade do cliente: \n");
-            fflush(stdin);
-            scanf("%i", &cl[i].idade);
+            scanf("%d",&cl[i].idade);            
             printf("Digite sua data de nascimento: \n");
-            fflush(stdin);
-            scanf("%i%i%i", &cl[i].dia, &cl[i].mes, &cl[i].ano);
+            printf("Dia: \n");
+            scanf("%i", &cl[i].dia);
+            printf("Mês: \n");
+            scanf("%i", &cl[i].mes);
+            printf("Ano: \n");
+            scanf("%i", &cl[i].ano);
             fprintf(cli, "%s\n%s\n%d\n%d/%d/%d\n\n", cl[i].cpf, cl[i].nome, cl[i].idade, cl[i].dia, cl[i].mes, cl[i].ano);
         }
 
     fclose(cli);
     free(cl); // Libere a memória alocada dinamicamente
-    clientes();
+    void clientes();
 }
-void listarClientes(){
-    int n;
-    printf("Quantos clientes deseja listar: ");
-    scanf("%i", &n);
-    struct Cliente *cl = (struct Cliente *)malloc(n * sizeof(struct Cliente)); 
-    FILE *cli;
 
-    cli=fopen("clientes.txt", "a" );
+void listarClientes(){    
+    FILE *cli;  
+    char c, string1[12], string2[12];
+    int linhas = 0;
+    cli = fopen("clientes.txt", "r");
 
-    printf("Listar Clientes :\n");
-
-    while (fscanf(cli, "%s%s%d%d/%d/%d", aux.cpf, aux.nome, &aux.idade, &aux.dia, &aux.mes, &aux.ano) != EOF) {
-           printf("CPF: %s\n", aux.cpf);
-           printf("Nome: %s", aux.nome);
-           printf("Idade: %d\n", aux.idade);
-           printf("Data de Nascimento: %d/%d/%d\n", aux.dia, aux.mes, aux.ano);                       
-           printf("--------------------------------------------------");        
+    if (cli == NULL) {
+        printf("Erro na abertura do arquivo!\n");
+        exit(1);
     }
-    
-    fclose (cli);
-    free(cl);
-    clientes();
+
+    while (fread(&c, sizeof(char), 1, cli)) {
+        if (c == '\n') {
+            linhas++;
+        }
+    }
+
+  int i, j, k;
+
+  linhas = linhas / 5;
+
+  Cliente aux;
+  char ordenado[linhas][12];
+  char troca[12];
+
+  printf("----------------------------------------\n");
+  printf("Lista de Clientes Ordenada por CPF :\n");
+  printf("----------------------------------------\n");
+
+  rewind(cli);     
+
+  i = 0;
+
+  while (fscanf(cli, "%s%s%d%d/%d/%d", aux.cpf, aux.nome, &aux.idade, &aux.dia, &aux.mes, &aux.ano) != EOF) {               
+     for(int j =0; j < 12; j++)
+     {       
+       ordenado[i][j] = aux.cpf[j];
+     }    
+     i++;
+  }  
+  for(int i = 0; i < linhas; i++)
+  {
+      int menor = i;
+      for(int j =i+1; j < linhas; j++)
+      {
+          string1[0] = ordenado[j][0];          
+          string1[1] = ordenado[j][1];          
+          string1[2] = ordenado[j][2];          
+          string1[3] = ordenado[j][3];          
+          string1[4] = ordenado[j][4];          
+          string1[5] = ordenado[j][5];          
+          string1[6] = ordenado[j][6];          
+          string1[7] = ordenado[j][7];          
+          string1[8] = ordenado[j][8];          
+          string1[9] = ordenado[j][9];          
+          string1[10] = ordenado[j][10];          
+          string1[11] = ordenado[j][11];          
+
+          string2[0] = ordenado[menor][0];
+          string2[1] = ordenado[menor][1];
+          string2[2] = ordenado[menor][2];
+          string2[3] = ordenado[menor][3];
+          string2[4] = ordenado[menor][4];
+          string2[5] = ordenado[menor][5];
+          string2[6] = ordenado[menor][6];
+          string2[7] = ordenado[menor][7];
+          string2[8] = ordenado[menor][8];
+          string2[9] = ordenado[menor][9];
+          string2[10] = ordenado[menor][10];
+          string2[11] = ordenado[menor][11];
+
+          if (string1[0] < string2[0])
+            menor = j;             
+      }              
+      if (menor != i)
+      {           
+           for(int j =0; j < 12; j++)
+           {          
+              troca[j] = ordenado[i][j];
+           }
+           for(int j =0; j < 12; j++)
+           {          
+              ordenado[i][j] = ordenado[menor][j];
+           }        
+           for(int j =0; j < 12; j++)
+           {          
+              ordenado[menor][j] = troca[j];
+           }
+      }
+  }
+  for(k = 0; k < linhas; k++)
+  {
+    printf("CPF %d:%s\n",k+1,ordenado[k]);    
+  }
+  printf("----------------------------------------\n");
+  fclose (cli);      
+  void clientes();
 }
 void consultarCliente(){
   char cpfConsulta[12];
-    printf("Digite o CPF do cliente que deseja consultar: ");
-    fflush(stdin);
-    fgets(cpfConsulta, 12, stdin);
+  printf("----------------------------------------\n");
+  printf("Digite o CPF do cliente a consultar: ");
+
+    scanf("%s", cpfConsulta);
 
     FILE *cli;
     cli = fopen("clientes.txt", "r");
@@ -159,12 +227,12 @@ void consultarCliente(){
         exit(1);
     }
 
-    struct Cliente aux;
+    Cliente aux;
 
     while (fscanf(cli, "%s%s%d%d/%d/%d", aux.cpf, aux.nome, &aux.idade, &aux.dia, &aux.mes, &aux.ano) != EOF) {
         if (strcmp(aux.cpf, cpfConsulta) == 0) {
             printf("CPF: %s\n", aux.cpf);
-            printf("Nome: %s", aux.nome);
+            printf("Nome: %s\n", aux.nome);
             printf("Idade: %d\n", aux.idade);
             printf("Data de Nascimento: %d/%d/%d\n", aux.dia, aux.mes, aux.ano);
             fclose(cli);
@@ -174,13 +242,14 @@ void consultarCliente(){
 
     printf("Cliente não encontrado!\n");
     fclose(cli);
-
+    void clientes();
 }
 void desativarCliente(){
   char cpfDesativar[12];
-    printf("Digite o CPF do cliente que deseja desativar: ");
-    fflush(stdin);
-    fgets(cpfDesativar, 12, stdin);
+  printf("----------------------------------------\n");
+  printf("Digite o CPF do cliente para desativar: ");
+  fflush(stdin);
+  fgets(cpfDesativar, 12, stdin);
 
     FILE *cli;
     FILE *temp;
@@ -193,7 +262,7 @@ void desativarCliente(){
         exit(1);
     }
 
-    struct Cliente aux;
+    Cliente aux;
 
     while (fscanf(cli, "%s%s%d%d/%d/%d", aux.cpf, aux.nome, &aux.idade, &aux.dia, &aux.mes, &aux.ano) != EOF) {
         if (strcmp(aux.cpf, cpfDesativar) == 0) {
@@ -209,13 +278,14 @@ void desativarCliente(){
 
     remove("clientes.txt");
     rename("temp.txt", "clientes.txt");
-    clientes();
+    void clientes();
 }
 void excluirCliente(){
   char cpfExcluir[12];
-    printf("Digite o CPF do cliente que deseja excluir: ");
-    fflush(stdin);
-    fgets(cpfExcluir, 12, stdin);
+  printf("----------------------------------------\n");
+  printf("Digite o CPF do cliente que deseja excluir: ");
+  fflush(stdin);
+  fgets(cpfExcluir, 12, stdin);
 
     FILE *cli;
     FILE *temp;
@@ -228,7 +298,7 @@ void excluirCliente(){
         exit(1);
     }
 
-    struct Cliente aux;
+    Cliente aux;
 
     while (fscanf(cli, "%s%s%d%d/%d/%d", aux.cpf, aux.nome, &aux.idade, &aux.dia, &aux.mes, &aux.ano) != EOF) {
         if (strcmp(aux.cpf, cpfExcluir) != 0) {
@@ -243,19 +313,20 @@ void excluirCliente(){
     rename("temp.txt", "clientes.txt");
 
     printf("Cliente excluido com sucesso!\n");
-    clientes();
+    void clientes();
 }
 void cadastrarAnimal(){
-    int n;
-    printf("Cadastrando Animais :\n");
-    printf("Quantos animais deseja cadastrar: ");
-    scanf("%i", &n);
+  int n;
+  printf("----------------------------------------\n");
+  printf("Cadastrando Animais :\n");
+  printf("Quantos animais deseja cadastrar: ");
+  scanf("%i", &n);
 
-    struct Animal *an = (struct Animal *)malloc(n * sizeof(struct Animal)); // Alocação dinâmica
+  Animal *an = (Animal *)malloc(n * sizeof(Animal));
 
     if (an == NULL) {
         printf("Falha na alocacao de memoria.\n");
-        return 1;
+        return;
     }
 
     FILE *ani;
@@ -278,19 +349,20 @@ void cadastrarAnimal(){
             fgets(an[i].cor, 10, stdin);
             strcpy(an[i].status, "Ativo");
             contador++;
-            fwrite(&an[i], sizeof(struct Animal), 1, ani);
+            fwrite(&an[i], sizeof(Animal), 1, ani);
         }
 
     fclose(ani);
     free(an); // Libere a memória alocada dinamicamente
-    animais();
+    void animais();
 }
 void listarAnimais(){
   int n;
-    printf("Quantos animais deseja listar: ");
-    scanf("%i", &n);
+  printf("----------------------------------------\n");
+  printf("Quantos animais deseja listar: ");
+  scanf("%i", &n);
 
-    struct Animal *an = (struct Animal *)malloc(n * sizeof(struct Animal));
+  Animal *an = (Animal *)malloc(n * sizeof(Animal));
 
     FILE *ani;
     ani = fopen("animais.txt", "r");
@@ -302,7 +374,7 @@ void listarAnimais(){
 
     printf("Listar Animais:\n");
     for (int i = 0; i < n; i++) {
-        fread(&an[i], sizeof(struct Animal), 1, ani);
+        fread(&an[i], sizeof(Animal), 1, ani);
         printf("Código: %d\n", an[i].codigo);
         printf("Nome: %s", an[i].nome);
         printf("Idade: %d\n", an[i].idade);
@@ -312,12 +384,13 @@ void listarAnimais(){
 
     fclose(ani);
     free(an);
-    animais();
+    void animais();
 }
 void desativarAnimal(){
   int codigo;
-    printf("Digite o codigo do animal a ser desativado: ");
-    scanf("%d", &codigo);
+  printf("----------------------------------------\n");
+  printf("Digite o codigo do animal a ser desativado: ");
+  scanf("%d", &codigo);
 
     FILE *ani;
     FILE *temp;
@@ -330,11 +403,11 @@ void desativarAnimal(){
         exit(1);
     }
 
-    struct Animal aux;
+    Animal aux;
 
-    while (fread(&aux, sizeof(struct Animal), 1, ani)) {
+    while (fread(&aux, sizeof(Animal), 1, ani)) {
         if (aux.codigo != codigo) {
-            fwrite(&aux, sizeof(struct Animal), 1, temp);
+            fwrite(&aux, sizeof(Animal), 1, temp);
         }
     }
 
@@ -345,14 +418,11 @@ void desativarAnimal(){
     rename("temp.txt", "animais.txt");
 
     printf("Animal desativado com sucesso!\n");
-
-    animais();
+    void animais();
 }
 
 void clientes(){
 int opcao;
-
-
   while (1) {
          printf("-Escolha uma das opcoes abaixos-\n");
         printf("1. Cadastrar Cliente\n");
@@ -390,9 +460,10 @@ int opcao;
 }
 
 void adotarAnimal() {
-    int codigo;
-    printf("Digite o codigo do animal a ser adotado: ");
-    scanf("%d", &codigo);
+  int codigo;
+  printf("----------------------------------------\n");
+  printf("Digite o codigo do animal a ser adotado: ");
+  scanf("%d", &codigo);
 
     FILE *ani;
     FILE *temp;
@@ -405,11 +476,11 @@ void adotarAnimal() {
         exit(1);
     }
 
-    struct Animal aux;
+    Animal aux;
 
-    while (fread(&aux, sizeof(struct Animal), 1, ani)) {
+    while (fread(&aux, sizeof(Animal), 1, ani)) {
         if (aux.codigo != codigo) {
-            fwrite(&aux, sizeof(struct Animal), 1, temp);
+            fwrite(&aux, sizeof(Animal), 1, temp);
         } else {
             printf("Animal adotado com sucesso!\n");
         }
@@ -420,14 +491,12 @@ void adotarAnimal() {
 
     remove("animais.txt");
     rename("temp.txt", "animais.txt");
-   
+    void animais();
 }
 
 
 void animais(){
 int opcao;
-
-
   do {
         printf("-Escolha uma das opcoes abaixos-\n");
         printf("1. Cadastrar Animal\n");
